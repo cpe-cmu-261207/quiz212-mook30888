e4add6dd597c5005ad2d9f97294f40575a4065f9
mook30888 <72993985+mook30888@users.noreply.github.com>
2021-02-02 13:59:12 +0700

    time is gone

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 function App() {
   const persons = [
     {
@@ -11,34 +12,59 @@ function App() {
       age: "20"
     }
   ];
-  return (
-    <div class="ml-2">
-      {/* Code me please! */}
-      <div class="mb-4">
-        <h3 class="title is-3">ID Counter</h3>
-        <p>YOUR ID HERE</p>
-        <button>-</button>
-        <button>reset</button>
-        <button>+</button>
-      </div>
+  const [myIDnew, setMyID] = useState([]);
+  let myID = 620610773;
 
-      {/* Convert me to a component! */}
-      <h3 class="title is-3">Person List</h3>
-      <table class="table is-bordered mb-3">
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Age</th>
-          </tr>
-          <tr>
-            <td>Bob</td>
-            <td>male</td>
-            <td>50</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+  function minusID(myid) {
+    myid -= 1;
+  }
+  function plusID(myid) {
+    myid += 1;
+  }
+  function resetID(myid) {
+    myid = 620610773;
+  }
+
+  return (
+    <form
+      minus={(myID) => {
+        minusID(myID);
+      }}
+      plus={(myID) => {
+        plusID(myID);
+      }}
+      reset={(myID) => {
+        resetID(myID);
+      }}
+    >
+      <div class="ml-2">
+        {/* Code me please! */}
+
+        <div class="mb-4">
+          <h3 class="title is-3">ID Counter</h3>
+          <p>{myID}</p>
+          <button type="minus">-</button>
+          <button type="reset">reset</button>
+          <button type="plus">+</button>
+        </div>
+        {/* Convert me to a component! */}
+        <h3 class="title is-3">Person List</h3>
+        <table class="table is-bordered mb-3">
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Gender</th>
+              <th>Age</th>
+            </tr>
+            <tr>
+              <td>Bob</td>
+              <td>male</td>
+              <td>50</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </form>
   );
 }
 
